93cc5ab277179dc8e9a97abf82d6c93b60b28a18
Pablo Vinícius Pereira de Sá <56324757+Pablo-Sa@users.noreply.github.com>
2020-09-18 10:47:49 -0300

    Iniciando Projeto

---
+++ src/main/java/br/com/softplan/poc/SoftplanApplication.java
@@ -0,0 +1,13 @@
+package br.com.softplan.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softplan/poc/SoftplanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softplan.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
