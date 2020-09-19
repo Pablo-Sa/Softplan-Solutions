c55ec89843be9d8ba53312293821b39eecd28e85
Pablo Vinícius Pereira de Sá <56324757+Pablo-Sa@users.noreply.github.com>
2020-09-18 22:49:59 -0300

    Criado Interceptor e Serviço de Manipula o Token JWT

--- Front-End-Angular/src/app/app.module.ts
+++ Front-End-Angular/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { Interceptor } from './interceptors/interceptor.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    Interceptor,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
